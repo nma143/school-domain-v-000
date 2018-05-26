196693d56e3d0a55ed833b4551421bd2d153d8b5
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 18:49:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    @roster[grade] = []
+    @roster[grade] = [] unless @roaster.keys.include?(grade) end
     @roster[grade] << student_name
   end
 
